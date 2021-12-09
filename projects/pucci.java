2fccc16ebdee82f98dc9c743cc2c9f6083cbe3ea
Triple-Edged <usan.siri.trzm@gmail.com>
2021-12-08 22:25:25 -0500

    modified:   projects/pucci.java

--- projects/pucci.java
+++ projects/pucci.java
@@ -1,13 +1,22 @@
+import javax.swing.text.StyledEditorKit;
+
 import lejos.nxt.Button;
 
 public class pucci {
 
-    public static int primesTo = 10000;
     public static void main(String[] args) {
+
+        int primesTo = 1000;    // largest number to check for prime-ness
+        long startTime = System.currentTimeMillis(); // store time at start
+
         for (int i = 2; i <= primesTo; i++) {
             if (isPrimeBrute(i)){System.out.println(i);}
         }
-        Button.waitForAnyPress();
+
+        System.out.print(System.currentTimeMillis() - startTime); // print total time taken
+        System.out.print(" milis");
+
+        Button.waitForAnyPress();   // wait for button press after program finishes
     }
 
     public static boolean isPrimeBrute(int number){
