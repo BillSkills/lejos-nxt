e107fe7499d9a16c3ec335af3340644b5e996359
Triple-Edged <usan.siri.trzm@gmail.com>
2021-12-10 11:09:03 -0500

    modified:   projects/Jukebox/jukebox.java

---
+++ projects/Jukebox/jukebox.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+import lejos.nxt.*;
+import lejos.robotics.*;
+import lejos.util.*;
+
+public class jukebox {
+    public static void main(String[] args) {
+        File music = new File("moonmen.wav");
+        Sound.playSample(music);
+        Button.waitForAnyPress();
+    }
+    
+}
