5d133e6bd1796ba237ad2bc141e8c599046a122e
BillSkills <billskills01@gmail.com>
2021-12-11 10:11:30 -0500

    modified:   projects/Jukebox/jukebox.java 	modified:   projects/template/template.java

--- projects/Jukebox/jukebox.java
+++ projects/Jukebox/jukebox.java
@@ -11,5 +11,4 @@ public class jukebox {
         Sound.playSample(music);
         Button.waitForAnyPress();
     }
-    
 }
--- projects/template/template.java
+++ projects/template/template.java
@@ -7,12 +7,6 @@ import lejos.util.*;
 
 public class template {
     public static void main(String[] args) {
-<<<<<<< HEAD:projects/music_test.java
-        File music = new File("moonmen.wav");
-        Sound.playSample(music);
-        Button.waitForAnyPress();
-=======
 
->>>>>>> f53ae98d6733b1086eda567a36bf4f9780758df1:projects/template/template.java
     }
 }
